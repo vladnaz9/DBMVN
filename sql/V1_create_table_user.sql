7ec1c6a9b670103f0456f8ae42286f10bece9b19
NikitaRomanchuk <61383494+NikitaRomanchuk@users.noreply.github.com>
2020-09-17 09:46:12 +0300

    Add files via upload

---
+++ sql/V1_create_table_user.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `user` (
+  `username` varchar(16) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(32) NOT NULL,
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` int DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
