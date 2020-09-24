30233fa229e921a1182cb52b9aef5fb4a2bb07d9
Nazarychev_905 <vladnaz9@mail.ru>
2020-09-24 12:29:57 +0300

    V4

---
+++ sql/V4__teachers_create.sql
@@ -0,0 +1,6 @@
+
+CREATE TABLE `db1`.`teachers` (
+  `teachers_id` INT DEFAULT NULL,
+UNIQUE KEY teachers_id_UNIQUE (teachers_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  
